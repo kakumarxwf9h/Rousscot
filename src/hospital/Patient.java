919d0365419610bab816830dbcefeb5091589be4
kakumarxwf9h <pangpanmao336@163.com>
2021-05-29 20:06:56 +0800

    added forst classes

---
+++ src/hospital/Patient.java
@@ -0,0 +1,7 @@
+package hospital;
+
+/**
+ * Created by ferlicotdelbe on 23/10/15.
+ */
+public class Patient {
+}
---
+++ test/PatientTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by ferlicotdelbe on 23/10/15.
+ */
+public class PatientTest {
+}
