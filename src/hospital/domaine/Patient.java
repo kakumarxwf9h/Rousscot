496f2fefdcf2f5a96be64ca8b6547aca702bea5d
kakumarxwf9h <pangpanmao336@163.com>
2021-06-04 06:03:00 +0800

    begun the patient

--- src/hospital/domaine/Patient.java
+++ src/hospital/domaine/Patient.java
@@ -4,4 +4,19 @@ package hospital.domaine;
  * Created by ferlicotdelbe on 23/10/15.
  */
 public class Patient {
+
+    protected String last_name;
+
+    protected Long numSS;
+
+    protected String address;
+
+    protected Integer age;
+
+    public Patient(String name, Long numSS, String address, Integer age) {
+        this.address = address;
+        this.last_name = name;
+        this.age = age;
+        this.numSS = numSS;
+    }
 }
--- src/hospital/factory/PatientFactory.java
+++ src/hospital/factory/PatientFactory.java
@@ -1,7 +1,23 @@
 package hospital.factory;
 
+import hospital.domaine.Patient;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by ferlicotdelbe on 23/10/15.
  */
 public class PatientFactory {
+    private static PatientFactory INSTANCE = new PatientFactory();
+
+    public static PatientFactory current() {
+        return INSTANCE;
+    }
+
+    protected Set<Patient> patients;
+
+    private PatientFactory() {
+        this.patients = new HashSet<Patient>();
+    }
 }
