76467c343559e72a357679173f4fd4342766410d
kakumarxwf9h <pangpanmao336@163.com>
2021-07-04 00:17:18 +0800

    correct typo

--- src/hospital/metier/PatientExit.java
+++ src/hospital/metier/PatientExit.java
@@ -11,7 +11,7 @@ import java.io.IOException;
  */
 public class PatientExit extends Action {
 
-    protected final static String COMMAND = "exit";
+    protected final static String COMMAND = "sortie";
 
     /*
      * TODO
